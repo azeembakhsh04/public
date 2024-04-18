03eea2985b8899e421b765f8239c33848ad1769a
azeembakhsh04 <96291603+azeembakhsh04@users.noreply.github.com>
2024-04-18 17:38:09 +0500

    Update js

---
+++ js/price-calculator-update.js
@@ -0,0 +1,16 @@
+let text = 'New Version $url'
+let link = 'https://google.com/'
+let update = false;
+let txt;
+
+
+function appUpdate() {
+  if (update) {
+      let url = `<a href="${link}" target="_blank" style="text-decoration: none;"> Download apk </a>`
+      let displayText = text.replace('$url', url)
+      txt = ` <h4> ${displayText} </h4>`;
+  } else {
+       txt = '<h4> You have latest Update </h4>'
+  }
+  return txt;
+}
